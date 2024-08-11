56aa1e9abc29a6bda9da5723f4e1cf961c56a430
Yejin.Shin <mingmi_27@naver.com>
2024-08-12 00:43:12 +0900

    Feat : 디자인 확정 이전의 앱바, 홈화면 UI, 바텀네비게이션 작성

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:duogaza/presentaion/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,21 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('게시글'),
+          centerTitle: true,
+        ),
+        body: HomeScreen(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: '게시판'),
+            BottomNavigationBarItem(icon: Icon(Icons.whatshot), label: '빠른매칭'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle), label: '내계정')
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/presentaion/home/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:duogaza/presentaion/home/component/board_menu.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          BoardMenu(),
+          Expanded(
+            child: ListView(
+              cacheExtent: 100,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('40대 중반 롤 친구 구해요'),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.more_vert,
+                              size: 20,
+                            ),
+                          )
+                        ],
+                      ),
+                      Text('40대 중반 친구 구해요 얄라얄리 얄라셩 가라뫼 삼거리 아래 개천 흐르는 곳 길냥이 집'),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('2024.04.01'),
+                          Text('댓글 10'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
